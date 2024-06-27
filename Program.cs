6d0aafb6480e7e7692ce05c4505302d1a74e5c2c
Mekilo <mekilodike89@gmail.com>
2024-06-27 15:12:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using CompanyEmployee.Extension;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello from the middleware component.");
+});
+
+app.MapControllers();
+
+app.Run();
